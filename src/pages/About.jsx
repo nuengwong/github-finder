56681a3ad8696feee2fa4808d006c2f6e24922a9
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-09-04 22:30:05 +0700

    08 GitHub Finder Project Start

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>A React app to search
+      GitHub profiles and see profile details. This project is part of the{' '}
+      <a href="https://www.udemy.com/course/modern-react-front-to-back">
+        {' '}
+        React Front To Back
+      </a>{' '}
+      Udemy course by
+      <strong>Brad Traversy</strong>.
+    </div>
+  );
+}
+
+export default About;
